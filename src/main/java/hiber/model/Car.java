0b28d14d9ec6ee15b69a33f523d6b478d85aac39
alena.goryunova <alena.goryunova@rtlabs.ru>
2024-01-06 18:48:31 +0300

    Добавлен класс Car

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,42 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer series;
+
+    @Column(name = "model")
+    private String model;
+
+    public Car() {}
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "series = " + series +
+                ", model = " + model;
+    }
+}
