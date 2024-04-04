1a67c1a2cc2c0a0da3237696a55eb1ba2b687f34
alena.goryunova <alena.goryunova@rtlabs.ru>
2024-04-04 12:11:20 +0300

    Комменты

--- src/main/java/hiber/config/AppConfig.java
+++ src/main/java/hiber/config/AppConfig.java
@@ -19,14 +19,14 @@ import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:db.properties")
-@EnableTransactionManagement
+@EnableTransactionManagement // Включает возможность управления транзакциями Spring на основе аннотаций
 @ComponentScan(value = "hiber")
 public class AppConfig {
 
    @Autowired
-   private Environment env;
+   private Environment env; // интерфейс для работы с Property
 
-   @Bean
+   @Bean //? прям нужна
    public DataSource getDataSource() {
       DriverManagerDataSource dataSource = new DriverManagerDataSource();
       dataSource.setDriverClassName(env.getProperty("hibernate.connection.driver_class"));
@@ -36,6 +36,8 @@ public class AppConfig {
       return dataSource;
    }
 
+   // создание SessionFactory (указываем на Entity классы, dataSource и Properties)
+   // фабрика entity объектов
    @Bean
    public LocalSessionFactoryBean getSessionFactory() {
       LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
@@ -50,6 +52,8 @@ public class AppConfig {
       return factoryBean;
    }
 
+   // управление транзакциями (перехватывает методы @Transactional)
+   // можете изменить способ управления транзакциями, просто изменив конфигурацию
    @Bean
    public HibernateTransactionManager getTransactionManager() {
       HibernateTransactionManager transactionManager = new HibernateTransactionManager();
